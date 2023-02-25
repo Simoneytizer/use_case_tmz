32fa7040603b27d712fcd288df34f57d1e08c63f
Audrey Malecki <audrey.malecki.am@gmail.com>
2023-02-25 17:23:15 +0100

    ongoing

--- use_case_tmz/api/fast.py
+++ use_case_tmz/api/fast.py
@@ -1,9 +1,54 @@
 from fastapi import FastAPI
-from use_case_tmz.ml_logic.models import load_model
+
+from use_case_tmz.ml_logic.data_preparation import all_data_for_one_site
+from use_case_tmz.ml_logic.preprocessing import preprocess_features
+from use_case_tmz.ml_logic.models import load_pipe_ensemble, predict_value
 
 app = FastAPI()
-# app.state.model = load_model()
+app.state.model = load_pipe_ensemble('pipe_ensemble_model')
 
 @app.get("/")
 def root():
     return {'greeting': 'Welcome to our project'}
+
+
+
+@app.get("/predict")
+def predict(site_url, blocklist_value=0, geo='Other', _1_=0, _2_=1, _3_=0, _4_=0, _5_=0,
+       _6_=0, _11_=0, _15_=0, _16_=0, _19_=0, _20_=0, _24_=0, _27_=0, _28_=0, _30_=0,
+       _31_=0, _34_=0, _38_=0, _39_=0, _43_=0, _44_=0, _46_=0):
+
+    X = all_data_for_one_site(site_url, blocklist_value, geo, _1_, _2_, _3_, _4_, _5_,
+       _6_, _11_, _15_, _16_, _19_, _20_, _24_, _27_, _28_, _30_,
+       _31_, _34_, _38_, _39_, _43_, _44_, _46_)
+
+    X_preproc = preprocess_features(X)
+
+    model = load_pipe_ensemble('pipe_ensemble_model')
+
+    y_pred = predict_value(X_preproc, model)
+
+    X = X.fillna('Not found')
+
+    return {'site_url': site_url,
+            'lighthouse_score': X['lighthouse_score'].values[0],
+            'LCP': X['LCP'][0],
+            'FID': X['FID'][0],
+            'CLS': X['CLS'][0],
+            'FCP': X['FCP'][0],
+            'INP': X['INP'][0],
+            'TTFB': X['TTFB'][0],
+            'Social': X['Social'].values[0],
+            'Mail': X['Mail'].values[0],
+            'Referrals': X['Referrals'].values[0],
+            'Search': X['Search'].values[0],
+            'Direct': X['Direct'].values[0],
+            'BounceRate': X['BounceRate'].values[0],
+            'PagePerVisit': X['PagePerVisit'].values[0],
+            'Category': X['Category'].values[0],
+            'EstimatedMonthlyVisits': X['EstimatedMonthlyVisits'].values[0],
+            'estimated_ca': y_pred}
+
+
+if __name__=="__main__":
+    print(predict('https://delife.fun'))
--- use_case_tmz/ml_logic/models.py
+++ use_case_tmz/ml_logic/models.py
@@ -1,8 +1,11 @@
 import joblib
 import os
+import numpy as np
 
+from use_case_tmz.ml_logic.data_preparation import all_data_for_one_site
+from use_case_tmz.ml_logic.preprocessing import preprocess_features
 
-def load_model(file_name):
+def load_pipe_ensemble(file_name):
 
     root = os.path.dirname(os.path.dirname(__file__))
 
@@ -14,7 +17,19 @@ def load_model(file_name):
 
 
 
+def predict_value(X_preproc, pipe_model):
+    y_pred = pipe_model.predict(X_preproc)
+
+    return np.exp(y_pred)[0]
+
+
+
+
 
 
 if __name__=="__main__":
-    print(load_model('stacking_model'))
+    # print(load_pipe_ensemble('pipe_ensemble_model'))
+    df_test = all_data_for_one_site('https://www.coursfrancaisfacile.com', 1, 'US', _19_=1, _46_=1)
+    X_preproc = preprocess_features(df_test)
+    model = load_pipe_ensemble('pipe_ensemble_model')
+    print(predict_value(X_preproc, model))
