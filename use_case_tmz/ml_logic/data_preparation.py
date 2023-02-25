165a2c9342f60f482a1f6c9ec616cdf8e2e3f67b
Audrey Malecki <audrey.malecki.am@gmail.com>
2023-02-25 11:27:54 +0100

    modif in data_prep to include nb_formats

--- use_case_tmz/ml_logic/data_preparation.py
+++ use_case_tmz/ml_logic/data_preparation.py
@@ -78,6 +78,11 @@ def ads_format_selected(url, format_1=0, format_2=0, format_3=0, format_4=0, for
 
 
 
+def number_of_formats(df):
+    num_values = df.select_dtypes(include='int')
+    return num_values.values.sum()
+
+
 
 def top_geo(geo=np.NaN):
     return geo
@@ -99,6 +104,8 @@ def all_data_for_one_site(url, blocklist_value, geo, format_1=0, format_2=0, for
        format_6, format_11, format_15, format_16, format_19, format_20, format_24, format_27, format_28, format_30,
        format_31, format_34, format_38, format_39, format_43, format_44, format_46)
 
+    df['nb_formats'] = number_of_formats(df_format)
+
     df = pd.merge(df, df_format, on='site_url', how='left')
 
     return df
@@ -110,3 +117,4 @@ if __name__ == '__main__':
     # get_data_from_both_apis('https://dailyloannews.com')
     # ads_format_selected('https://dailyloannews.com', format_44=1, format_11=1)
     print(all_data_for_one_site('https://www.coursfrancaisfacile.com', 1, 'US', format_19=1, format_46=1))
+    # print(number_of_formats(ads_format_selected('https://dailyloannews.com', format_44=1, format_11=1)))
